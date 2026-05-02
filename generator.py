caf3f321541805dd15017862de8b76f2722f8fda
tkolecki <tkolecki@gmail.com>
2026-05-02 21:34:38 +0200

    Add CLI mode with argparse and fix use_upper logic

--- generator.py
+++ generator.py
@@ -1,5 +1,6 @@
 import string
 import random
+import argparse
 
 #print(string.ascii_letters)
 #print(string.digits)
@@ -10,7 +11,7 @@ import random
 def generate_password(length, use_digits=True, use_special=True, use_upper=True):
     #characters = string.ascii_letters + string.digits + string.punctuation
     characters = string.ascii_letters
-    if use_upper:   characters = characters.lower()
+    if not use_upper:   characters = characters.lower()
     if use_digits:  characters += string.digits
     if use_special: characters += string.punctuation
     
@@ -20,21 +21,33 @@ def generate_password(length, use_digits=True, use_special=True, use_upper=True)
     return password
 
 def main():
-    password_dict = dict()
     print("--- PASSWORD GENERATOR ---")
-    suggestion_count = int(input("How many password suggestions you want? "))
-    
-    for i in range(1, suggestion_count+1):
-        length = random.randint(8, 20)
-        password_dict[i] = generate_password(length)
 
-    for k, v in password_dict.items():
-        print(f"{k}: {v}") 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--length", type=int, default=None)
+    parser.add_argument("--no_digits", action="store_false", dest="use_digits")
+    parser.add_argument("--no_special", action="store_false", dest="use_special")
+    parser.add_argument("--no_upper", action="store_false", dest="use_upper")
+    args = parser.parse_args()
 
-    password_to_save = int(input(f"Which one to save 1-{suggestion_count}: "))
-    
+    if args.length is None:
+        # no arguments given basic mode
+        length = int(input("Password length: "))
+        digits = input("Contains digits (Y/N - default Y): ").strip().upper()
+        use_digits = digits != "N"
+        special = input("Contains special characters (Y/N - default Y): ").strip().upper()
+        use_special = special != "N"
+        upper = input("Contains upper cases (Y/N - default Y): ").strip().upper()
+        use_upper = upper != "N"
+        
+        password_to_save = generate_password(length=length, use_digits=use_digits, use_special=use_special, use_upper=use_upper)
+
+    else:
+        password_to_save = generate_password(args.length, use_digits=args.use_digits, use_special=args.use_special, use_upper=args.use_upper)
+        print(f"Generated password: {password_to_save}\n Saving to file...")
+
     with open("password.txt", "w") as f:
-        f.write(password_dict[password_to_save])
+        f.write(password_to_save)
 
 if __name__ == "__main__":
     main()
