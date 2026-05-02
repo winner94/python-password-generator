5767084e130aeda1c8ed381ae637095bdfcc98c3
tkolecki <tkolecki@gmail.com>
2026-05-02 11:37:10 +0200

    Add options for digits, uppercase, punctuation in password generator

--- generator.py
+++ generator.py
@@ -7,11 +7,19 @@ import random
 #print(random.choice("abcdef"))
 #
 
-def generatore_password(length):
-    characters = string.ascii_letters + string.digits + string.punctuation
+def generatore_password(length, use_digits=True, use_special=True, use_upper=True):
+    #characters = string.ascii_letters + string.digits + string.punctuation
+    characters = string.ascii_letters
+    if use_upper:   characters = characters.lower()
+    if use_digits:  characters += string.digits
+    if use_special: characters += string.punctuation
+    
     password = ""
     for i in range(length):
         password += random.choice(characters)
     return password
 
 print(generatore_password(8))
+print(generatore_password(12, use_digits=False))
+print(generatore_password(8, use_special=False))
+print(generatore_password(8, use_upper=False))
